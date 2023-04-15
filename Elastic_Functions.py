33eaacf50254ff21e961ca430c964024acdd8cca
Horep <horep245@gmail.com>
2023-04-15 16:22:17 +0100

    Commented out code that prevents successful run
    I have removed part of the testing code, since it prevented a successful run through of the code. This means testing if the displaying magnetisation function works properly is possible just by running it once. I also changed up how the strain and stress functions are related. Now you just input a generic strain into the stress function, which means that including the magnetostrain will be as simple as taking a difference within the stress function.

--- Elastic_Functions.py
+++ Elastic_Functions.py
@@ -6,6 +6,10 @@ import numpy as np
 mu = 1
 lam = 1
 
-def Stress(u):
-    strain = Sym(grad(u))
+
+def Strain_el(u):
+    return Sym(grad(u))
+
+
+def Stress(strain):
     return 2*mu*strain + lam*Trace(strain)*Id(3)
