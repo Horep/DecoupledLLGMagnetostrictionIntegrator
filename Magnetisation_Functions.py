2a2f9bdbf2ed92fe5a311097927344e08d0440b3
Horep <horep245@gmail.com>
2023-04-21 23:00:28 +0100

    Implemented magnetic energy

--- Magnetisation_Functions.py
+++ Magnetisation_Functions.py
@@ -6,6 +6,7 @@ import scipy.sparse as sp
 import matplotlib.pyplot as plt
 import General_Functions as genfunc
 
+
 def give_random_magnetisation(mag_grid_func):
     """
     Returns a random normalised magnetisation grid function.
@@ -121,9 +122,7 @@ def build_strain_m(fes_eps_m, mag_grid_func):
     numpoints = genfunc.get_num_nodes(mag_grid_func)
     m1, m2, m3 = mag_grid_func.components
     mymatrix = GridFunction(fes_eps_m)
-    M11, M12, M13,\
-    M21, M22, M23,\
-    M31, M32, M33 = mymatrix.components
+    M11, M12, M13, M21, M22, M23, M31, M32, M33 = mymatrix.components
     # this is a bad implementation, should be broadcast using numpy arrays, and use symmetry of the matrix. I have avoided this as it makes the code less readable
     # the symmetry can be implemented in the finite element space fes_eps_m directly with the flag symmetry=True
 
@@ -141,8 +140,27 @@ def build_strain_m(fes_eps_m, mag_grid_func):
     return mymatrix
 
 
-def build_magnetic_lin_system(fes_mag, mag_gfu, fes_eps_m, ALPHA: float, THETA: float, K: float, KAPPA:float):
-    #magstrain = build_strain_m(fes_eps_m, mag_gfu)
+def build_magnetic_lin_system(
+    fes_mag, mag_gfu, fes_eps_m, ALPHA: float, THETA: float, K: float, KAPPA: float
+):
+    """
+    Builds the variational formulation in terms of H1 components.
+    Does not account for unit length constraint.
+
+    Parameters:
+        fes_mag (ngsolve.comp.VectorH1): VectorH1 finite element space.
+        mag_gfu (ngsolve.comp.GridFunction): A VectorH1 grid function.
+        fes_eps_m (ngsolve.comp.MatrixValued): Matrix finite element space.
+        ALPHA (float): Dissipative constant in LLG equation.
+        THETA (float): Implicitness parameter.
+        K (float): Time step.
+        KAPPA (float): Relative strength of magnetic vs. elastic contributions.
+
+    Returns:
+        a_mag (ngsolve.comp.BilinearForm): The 3Nx3N assembled magnetisation "stiffness" matrix.
+        f_mag (ngsolve.comp.LinearForm): The 3Nx1 assembled force vector.
+    """
+    # magstrain = build_strain_m(fes_eps_m, mag_gfu)
     # Test functions
     v = fes_mag.TrialFunction()
     phi = fes_mag.TestFunction()
@@ -161,30 +179,47 @@ def build_magnetic_lin_system(fes_mag, mag_gfu, fes_eps_m, ALPHA: float, THETA: 
     return a_mag, f_mag
 
 
-def update_magnetisation(fes_mag, mag_gfu, fes_eps_m, ALPHA: float, THETA: float, K: float, KAPPA:float):
+def update_magnetisation(
+    fes_mag, mag_gfu, fes_eps_m, ALPHA: float, THETA: float, K: float, KAPPA: float
+):
     """
     Updates a magnetisation vector with the new values.
+
     Parameters:
-        fes_mag (ngsolve.comp.VectorH1)
-        mag_gfu (ngsolve.comp.BilinearForm): The 3Nx3N assembled magnetisation "stiffness" matrix from the variational formulation.
-        fes_eps_m (ngsolve.comp.MatrixValued)
-        ALPHA (float)
-        THETA (float)
-        K (float)
-        KAPPA (float)
+        fes_mag (ngsolve.comp.VectorH1): VectorH1 finite element space.
+        mag_gfu (ngsolve.comp.GridFunction): A VectorH1 grid function.
+        fes_eps_m (ngsolve.comp.MatrixValued): Matrix finite element space.
+        ALPHA (float): Dissipative constant in LLG equation.
+        THETA (float): Implicitness parameter.
+        K (float): Time step.
+        KAPPA (float): Relative strength of magnetic vs. elastic contributions.
 
     Returns:
         mag_grid_func (numpy.ndarray): The new updated magnetisation at the next time step.
-
     """
-    a_mag, f_mag = build_magnetic_lin_system(fes_mag, mag_gfu, fes_eps_m, ALPHA, THETA, K, KAPPA)
+    a_mag, f_mag = build_magnetic_lin_system(
+        fes_mag, mag_gfu, fes_eps_m, ALPHA, THETA, K, KAPPA
+    )
     B = build_tangent_plane_matrix(mag_gfu)
     v = give_magnetisation_update(a_mag, B, f_mag)
 
     N = genfunc.get_num_nodes(mag_gfu)
     mag_gfux, mag_gfuy, mag_gfuz = mag_gfu.components
-    mag_gfux.vec.FV().NumPy()[:] += K*v[0:N]
-    mag_gfuy.vec.FV().NumPy()[:] += K*v[N : 2 * N]
-    mag_gfuz.vec.FV().NumPy()[:] += K*v[2 * N : 3 * N]
+    mag_gfux.vec.FV().NumPy()[:] += K * v[0:N]
+    mag_gfuy.vec.FV().NumPy()[:] += K * v[N : 2 * N]
+    mag_gfuz.vec.FV().NumPy()[:] += K * v[2 * N : 3 * N]
 
     return mag_gfu
+
+
+def magnetic_energy(mag_gfu, mesh) -> float:
+    """
+    Returns 1/2 _/‾||∇m||^2 dx
+    Parameters:
+        mag_gfu (ngsolve.comp.GridFunction): A VectorH1 grid function.
+        mesh (ngsolve.comp.Mesh)
+
+    Returns:
+        mag_grid_func (numpy.ndarray): The new updated magnetisation at the next time step.
+    """
+    return 0.5 * Integrate(InnerProduct(Grad(mag_gfu), Grad(mag_gfu)), mesh, VOL)
