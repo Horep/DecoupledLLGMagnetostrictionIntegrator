1146a9b6eb6ccb948d07e6c15d0474fc7031b3b8
Horep <horep245@gmail.com>
2023-04-21 16:48:55 +0100

    Implemented ceiling division

--- General_Functions.py
+++ General_Functions.py
@@ -4,7 +4,7 @@ import math
 import numpy as np
 
 
-def get_num_nodes(grid_func):
+def get_num_nodes(grid_func) -> int:
     """
     Returns the number of nodes within the grid function.
 
@@ -12,9 +12,28 @@ def get_num_nodes(grid_func):
         mag_grid_func (ngsolve.comp.GridFunction): A VectorH1 grid function.
 
     Returns:
-        number of nodes (Int): Number of nodes in grid function.
+        number of nodes (int): Number of nodes in grid function.
     """
     assert (
         len(grid_func.vec) % 3 == 0
     ), "The vector data is not a multiple of three. Wrong dimension?"
     return len(grid_func.vec) // 3
+
+
+def ceiling_division(a: float, b: float) -> int:
+    """
+    Returns the closest integer greater than a/b
+
+    Parameters:
+        a (float): The numerator.
+        b (float): The denominator.
+
+    Returns:
+        ceil(a/b) (int): Number of nodes in grid function.
+    
+    Examples:
+        ceiling_division(1.0, 2.0) = 1
+        ceiling_division(5.0, 4.0) = 2
+    """
+    result = int(-(a // -b))
+    return result
